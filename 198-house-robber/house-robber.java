2a223c381f8f19662f053a15dec90c78b1d29dd4
Daksh Patidar <patidardaksh1390@gmail.com>
2026-07-06 07:22:10 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (56.14%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int rob(int[] arr) {
+        int dp[] = new int[arr.length];
+        int n=arr.length;
+        dp[0]=arr[0];
+        if(n>1)dp[1]=Math.max(arr[0], arr[1]);
+        for(int i=2;i<arr.length;i++){
+            dp[i] = Math.max(arr[i]+dp[i-2], dp[i-1]);
+        }
+        return dp[n-1];
+    }
+}
