cb5fdeccbd895a68d7999835f778594b2cc92dfe
Daksh Patidar <patidardaksh1390@gmail.com>
2026-09-07 16:05:05 +0530

    Time: 24 ms (82.36%) | Memory: 60.9 MB (5.65%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,38 +1,12 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        int left=0,right=points.length-1;;
-        while(left<=right){
-            int pivotIdx = partition(points,left,right);
-            if(pivotIdx==k)break;
-            else if(pivotIdx<k)left=pivotIdx+1;
-            else right=pivotIdx-1;
-        }
+        Arrays.sort(points,(a,b)->{
+            int distA = a[0]*a[0]+a[1]*a[1];
+            int distB = b[0]*b[0]+b[1]*b[1];
 
-        return Arrays.copyOfRange(points, 0, k);
-    }
+            return Integer.compare(distA,distB);
+        });
 
-    private int partition(int points[][],int left,int right){
-        int pivot[] = points[right];
-        int pivotDist = dist(pivot);
-        int i=left;
-        for(int j=left;j<right;j++){
-            if(dist(points[j])<=pivotDist){
-                swap(points,i,j);
-                i++;
-            }
-        }
-
-        swap(points,i,right);
-        return i;
-    }
-
-    private int dist(int point[]){
-        return point[0]*point[0]+point[1]*point[1];
-    }
-
-    private void swap(int points[][],int i,int j){
-        int temp[] = points[i];
-        points[i]=points[j];
-        points[j]=temp;
+        return Arrays.copyOfRange(points,0,k);
     }
 }
