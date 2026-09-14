7363bbfe54c5d9a49aee12ce03640f9672251b91
Daksh Patidar <patidardaksh1390@gmail.com>
2026-09-14 16:15:26 +0530

    Time: 2 ms (81.60%) | Memory: 46.8 MB (27.95%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,64 +1,35 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> result = new ArrayList<>();
-        
-        char[][] board = new char[n][n];
-        for (char[] row : board) {
-            Arrays.fill(row, '.');
-        }
-        
-        solve(0, board, result);
-        return result;
+        List<List<String>> ans = new ArrayList<>();
+        char board[][] =  new char[n][n];
+        for(char[] row:board){Arrays.fill(row,'.');}
+        placeQueen(0, board,ans);
+        return ans;
     }
-    
-    private void solve(int row, char[][] board, List<List<String>> result) {
-        if (row == board.length) {
-            result.add(construct(board));
+
+    private void placeQueen(int i,char board[][],List<List<String>> ans){
+        if(i==board.length){
+            ans.add(makeBoard(board));
             return;
         }
-        
-        for (int col = 0; col < board.length; col++) {
-            if (isSafe(row, col, board)) {
-                board[row][col] = 'Q';
-                solve(row + 1, board, result);
-                board[row][col] = '.'; 
-            }
+        for(int col=0;col<board.length;col++){
+            if(! isValid(i, col, board))continue;
+            board[i][col]='Q';
+            placeQueen(i+1, board, ans);
+            board[i][col]='.';
         }
     }
-    
-    private boolean isSafe(int row, int col, char[][] board) {
-        int r = row;
-        int c = col;
-        
-        while (r >= 0) {
-            if (board[r][c] == 'Q') return false;
-            r--;
-        }
-    
-        r = row;
-        c = col;
-        while (r >= 0 && c >= 0) {
-            if (board[r][c] == 'Q') return false;
-            r--;
-            c--;
-        }
 
-        r = row;
-        c = col;
-        while (r >= 0 && c < board.length) {
-            if (board[r][c] == 'Q') return false;
-            r--;
-            c++;
-        }
-        
+    private boolean isValid(int row,int col,char[][] board){
+        for(int i=0;i<row;i++){if(board[i][col]=='Q')return false;}
+        for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){if(board[i][j]=='Q')return false;}
+        for(int i=row-1,j=col+1;i>=0 && j<board.length;i--,j++){if(board[i][j]=='Q')return false;}
         return true;
     }
-    
-    private List<String> construct(char[][] board) {
-        List<String> list = new ArrayList<>();
-        for (char[] row : board) {
-            list.add(new String(row));
-        }
-        return list;
+
+    private List<String> makeBoard(char board[][]){
+        List<String> curr = new ArrayList<>();
+        for(char row[]:board){curr.add(new String(row));}
+        return curr;
     }
 }
