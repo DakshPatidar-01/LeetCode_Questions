7d508f7d2482558e026e580bfc0782c84472e858
Daksh Patidar <patidardaksh1390@gmail.com>
2026-09-16 15:06:50 +0530

    Time: 2 ms (99.16%) | Memory: 46.5 MB (52.49%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -15,23 +15,19 @@
  */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(root==null && subRoot==null)return true;
-        if(root==null || subRoot==null)return false;
-        return checkSubRoot(root,subRoot);
+        if(root==null)return false;
+        if(root.val==subRoot.val){
+            if(checkSubRoot(root, subRoot))return true;
+        }
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+
     }
 
     private boolean checkSubRoot(TreeNode root,TreeNode subRoot){
-        if(root==null)return false;
-        if(root.val==subRoot.val){
-            if(solve(root, subRoot))return true;
-        }
-        return checkSubRoot(root.left, subRoot) || checkSubRoot(root.right, subRoot);
-        
-    }
-    private boolean solve(TreeNode root,TreeNode subRoot){
         if(root==null && subRoot==null)return true;
         if(root==null || subRoot==null)return false;
         if(root.val!=subRoot.val)return false;
-        return solve(root.left, subRoot.left) && solve(root.right, subRoot.right);
+        return checkSubRoot(root.left, subRoot.left) && checkSubRoot(root.right, subRoot.right);
+        
     }
 }
