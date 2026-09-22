15e343806f845d76297075d98275e8d5d0cd7573
Daksh Patidar <patidardaksh1390@gmail.com>
2026-09-22 15:16:18 +0530

    Time: 3 ms (87.36%) | Memory: 52.3 MB (58.18%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,12 +1,10 @@
 class Solution {
-    int ans=0;
     public int numIslands(char[][] grid) {
-        int n=grid.length,m=grid[0].length;
-        boolean [][] visted = new boolean[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]=='1'&& !visted[i][j]){
-                    checkLand(i, j, grid, visted);
+        int ans=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]=='1'){
+                    checkLand(i,j,grid);
                     ans++;
                 }
             }
@@ -14,13 +12,14 @@ class Solution {
         return ans;
     }
 
-    private void checkLand(int i,int j,char grid[][],boolean visted[][]){
-        if(i<0 || i>=grid.length || j<0|| j>=grid[0].length || grid[i][j]=='0')return;
-        if(visted[i][j])return;
-        visted[i][j]=true;
-        checkLand(i+1, j, grid, visted);
-        checkLand(i-1, j, grid, visted);
-        checkLand(i, j+1, grid, visted);
-        checkLand(i, j-1, grid, visted);
+
+    private void checkLand(int i,int j,char grid[][]){
+        if(i<0||j<0||i>=grid.length||j>=grid[0].length||grid[i][j]=='0')return;
+        if(grid[i][j]=='@')return;
+        grid[i][j]='@';
+        checkLand(i+1, j, grid);
+        checkLand(i-1, j, grid);
+        checkLand(i, j+1, grid);
+        checkLand(i, j-1, grid);
     }
 }
