c2da66bb117f22ea2a14ef8926a827ee7d0e851c
Daksh Patidar <patidardaksh1390@gmail.com>
2026-09-22 15:59:21 +0530

    Time: 1 ms (39.51%) | Memory: 47 MB (11.12%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,19 +1,24 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        if (image[sr][sc] == color) {
-            return image;
+        boolean flag = false;
+        for(int i=0;i<image.length;i++){
+            for(int j=0;j<image[0].length;j++){
+                if(i==sr&&j==sc){
+                    fillColor(i,j,image,color,image[sr][sc]);
+                    return image;
+                }
+            }
+            if(flag)break;
         }
-        solve(image,sr,sc,color,image[sr][sc]);
         return image;
     }
 
-    private static void solve(int[][] image,int i,int j,int newColor,int orignal){
-        if(i<0 || i>=image.length || j<0 || j>=image[0].length)return;
-        if(image[i][j]!=orignal)return;
-        image[i][j]=newColor;
-        solve(image, i+1, j, newColor, orignal);
-        solve(image, i-1, j, newColor, orignal);
-        solve(image, i, j+1, newColor, orignal);
-        solve(image, i, j-1, newColor, orignal);
+    private void fillColor(int i,int j,int image[][],int color,int orignal){
+        if(i<0||j<0||i>=image.length||j>=image[0].length || image[i][j]!=orignal || image[i][j]==color)return;
+        image[i][j]=color;
+        fillColor(i+1, j, image, color,orignal);
+        fillColor(i-1, j, image, color,orignal);
+        fillColor(i, j+1, image, color,orignal);
+        fillColor(i, j-1, image, color,orignal);
     }
 }
